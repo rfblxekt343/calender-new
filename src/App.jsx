ed4ef4359f407b87a2926b58b455a02cdef2ff6d
Alma Levy <almalevy100@gmail.com>
2025-06-18 16:41:48 +0300

    newest commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import WeeklyCalendar from "./components/WeeklyCalneder"
-
+import { useEffect } from 'react';
+import { useState } from "react";
 
 function App() {
+const [showAlert, setShowAlert] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowAlert(true);
+    }, 3000);
+
+    return () => clearTimeout(timer); // Cleanup the timer if the component unmounts
+  }, []);
+
   return (
-   <WeeklyCalendar/>
+   <>
+     <WeeklyCalendar/>
+     {showAlert && <div className="alert">Hello after 3 seconds!</div>}
+   </>
   )
 }
 
